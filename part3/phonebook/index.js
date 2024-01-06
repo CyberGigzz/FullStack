4d5b96ea10197cd447b5c9aa8e020778de5dccba
Giga <gigakatamadze0@gmail.com>
2024-01-06 23:21:04 +0400

    Data is fetched from database and we can save new documents to the database

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+
+const Person = require("./models/person");
 
 const app = express();
 
@@ -23,56 +26,32 @@ app.use(
   })
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/info", (request, response) => {
-  const numOfEntries = persons.length;
-
-  response.send(`<p>Phonebook has info for ${numOfEntries} people</p>
-  <p>${new Date()}</p>`);
+app.get("/info", (request, response, next) => {
+  Person.find({}).then((people) => {
+    response.send(
+      `<p>Phonebook has info for ${
+        people.length
+      } people</p><p>${new Date()}</p>`
+    );
+  });
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end();
+  });
 });
 
 function generateUniqueID() {
@@ -81,34 +60,24 @@ function generateUniqueID() {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
+app.post("/api/persons", (request, response, next) => {
+  const { name, number } = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
-  }
+  const person = new Person({
+    name: name,
+    number: number,
+  });
 
-  const existingPerson = persons.some((person) => person.name === body.name);
-
-  if (existingPerson) {
-    return response.status(400).json({
-      error: "Name must be unique",
-    });
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    important: Boolean(body.important) || false,
-    id: generateUniqueID(),
-  };
-
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 
