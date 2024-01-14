2d1a9243bf52918a9d4d83686b4e860611b59fd2
Giga <gigakatamadze0@gmail.com>
2024-01-14 19:24:57 +0400

    Added mongoose validation, custom or built in

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -68,9 +68,12 @@ app.post("/api/persons", (request, response, next) => {
     number: number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -81,7 +84,11 @@ app.put("/api/persons/:id", (request, response, next) => {
     number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
